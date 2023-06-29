50c111a14ca4b59c8ccb7f45cc8fd9bf22fa163f
Elijah Okello <elijahokello90@gmail.com>
2023-06-29 10:58:52 +0300

    fix event model

--- fass_web_server/models.py
+++ fass_web_server/models.py
@@ -7,14 +7,16 @@ class Event(models.Model):
     id = models.AutoField(primary_key=True)
     time = models.DateTimeField(null=True, blank=True, default=timezone.now)
     location = models.CharField(max_length=255, null=True, blank=True)
-    status = models.CharField(max_length=255, blank=True)
-    event_type = models.CharField(max_length=255, null=True, blank=True)
+    status = models.CharField(max_length=255, blank=True,default="Not Resolved")
+    event_type = models.CharField(max_length=255, null=True, blank=True, default="deforestation")
     sound_file = models.CharField(max_length=255, null=True, blank=True)
     device = models.ForeignKey('Device', on_delete=models.CASCADE, related_name='events')
 
     class Meta:
         db_table = "event"
 
+    def __str__(self):
+        return self.location
 
 class Device(models.Model):
 
@@ -26,6 +28,9 @@ class Device(models.Model):
 
     class Meta:
         db_table = "device"
+
+    def __str__(self):
+        return self.name
 
 
 class User(models.Model):
@@ -45,3 +50,6 @@ class Noification(models.Model):
 
     class Meta:
         db_table = "noification"
+
+    def __str__(self):
+        return self.content
